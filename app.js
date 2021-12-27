43e36df1c48f63398f72f17fe6a1c5063197ffaf
Benjamin Hollon <benjaminbhollon@gmail.com>
2021-12-27 13:23:47 +0800

    Add customzied feed and basic pages

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const express = require('express');
 const compression = require('compression');
 const bodyParser = require('body-parser');
 const basicAuth = require('express-basic-auth');
-const MarkdownIt = require('markdown-it');
 const cookieParser = require('cookie-parser');
-
-const md = new MarkdownIt({ html: true, typographer: true, linkify: true });
+const parser = new (require('rss-parser'))();
+const RSS = require('rss');
 
 // Import config
 const config = require('./config.json');
 const directory = require('./directory.json');
+
+const crud = require('@bibliobone/mongodb-crud').bind(config.mongodbURI, config.dbName);
 
 const app = express();
 
@@ -38,10 +39,97 @@ app.use((request, response, next) => {
 app.set('view engine', 'pug');
 app.set('views', './templates');
 
+// Projects homepage
+app.get('/projects/', async (request, response) => {
+  let projects = [];
+  await crud.findMultipleDocuments('projects', {}).then((result) => {
+    projects = result;
+  });
+  response.render('projects.pug', {
+    projects,
+    cookies: request.cookies,
+    config
+  });
+});
+
+// Customized RSS
+const feedLinks = {
+  swec: 'https://seewitheyesclosed.com/feed/',
+  verbguac: 'https://verboseguacamole.com/feed/',
+  masto: 'https://fosstodon.org/users/benjaminhollon.rss'
+}
+const feedCaches = {
+  swec: false,
+  verbguac: false,
+  masto: false
+};
+
+async function updateFeeds() {
+  for (var f of Object.keys(feedCaches)) {
+    if (feedCaches[f] === false) {
+      feedCaches[f] = (await parser.parseURL(feedLinks[f])).items;
+
+      setTimeout((async (f) => {
+        feedCaches[f] = false;
+        try {
+          updateFeeds();
+        } catch(err) {
+          console.warn(err);
+        }
+      }).bind(this, f), Math.floor((Math.random() /* (5 * 1000 * 60)*/) + (/*5 */ 1000 * 60)));
+    }
+  }
+}
+
+try {
+  updateFeeds();
+} catch(err) {
+  console.warn(err);
+}
+
+app.get('/feed/', async (request, response) => {
+  await updateFeeds();
+
+  const items = request.query.from
+    .split(',')
+    .map((f) => feedCaches[f])
+    .flat()
+    .sort((a, b) => {
+      return (new Date(b.pubDate)) - (new Date(a.pubDate))
+    })
+    .map(i => {
+      const newI = {...i};
+
+      newI.description = i.content;
+      newI.date = new Date(i.pubDate);
+
+      return newI;
+    });
+
+  const feed = new RSS({
+    title: 'Feeds from Benjamin Hollon',
+    description: 'A customized aggregation of feeds from multiple sites by Benjamin Hollon.',
+    feed_url: request.protocol + '://' + request.hostname + request.url,
+    site_url: request.protocol + '://' + request.hostname,
+    managingEditor: 'Benjamin Hollon <br3zls68l@mozmail.com>',
+    webMaster: 'Benjamin Hollon <br3zls68l@mozmail.com>',
+    copyright: 'Verbose Guacamole: CC BY-NC 4.0\nSee With Eyes Closed: CC BY-SA 4.0\nMastodon: Unknown. Research needed.',
+    language: 'en',
+  });
+
+  for (const item of items) {
+    feed.item(item);
+  }
+
+  response.setHeader('Content-type', 'application/xml+rss');
+  response.end(feed.xml());
+  //response.json(items);
+});
+
 // Errors
 app.use((request, response) => {
   response.status(404);
-  response.render('errors/404.pug', { cookies: request.cookies });
+  response.render('errors/404.pug', { cookies: request.cookies, config });
 });
 
 // Listen on port from config.json
---
+++ assets/js/generateFeed.js
@@ -0,0 +1,12 @@
+function generateFeedLink() {
+  q = (s) => document.querySelector(s)
+  const feeds = [
+    (!q('#swec-feed').checked || 'swec'),
+    (!q('#verbguac-feed').checked || 'verbguac'),
+    (!q('#masto-feed').checked || 'masto'),
+  ].filter(i => typeof i === 'string').join(',');
+
+  const link = 'https://benjaminhollon.com/feed/?from=' + encodeURIComponent(feeds);
+  q('#finalFeed').innerHTML = '<a href="' + link + '">' + link + '</a>';
+  q('#final').classList.remove('hidden');
+}
